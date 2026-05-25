4768108479e3b64edcdd2b831a80a25cb8426360
Maxim <deminmaxim14@gmail.com>
2026-05-26 00:22:54 +0300

    test yookassa

--- backend/app/services/yookassa_service.py
+++ backend/app/services/yookassa_service.py
@@ -25,6 +25,7 @@ def create_yookassa_payment(
             "return_url": settings.yookassa_return_url,
         },
         "capture": True,
+        "save_payment_method": True,
         "description": description,
         "receipt": {
             "customer": {
